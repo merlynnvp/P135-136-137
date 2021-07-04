c5441c972b7c23a9a08922ca0bf7aea017dcd930
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-07-04 15:56:19 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+objects = [];
+status = "";
 video = "";
+object_name = "";
 
 function preload()
 {
@@ -12,15 +15,11 @@ function setup()
      canvas.center();
 }
 
-function draw()
-{
-    image(video, 0, 0, 480, 380);
-}
-
 function start()
 {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
 }
 
 function modelLoaded()
@@ -30,4 +29,44 @@ function modelLoaded()
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results)
+{
+    if (error) 
+    {
+        console.log(error);
+        objects = results;
+    }
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i< objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Obejects Detected";
+            document.getElementById("number_of_objects").innerHTML = "number of objects detected are :" + objects.length;
+
+            fill("#FF0000");
+            percent =floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15 );
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if(objects[i].label == object_name) 
+            { 
+                video.stop(); objectDetector.detect(gotResult); 
+                document.getElementById("object_status").innerHTML = object_name + " Found"; 
+                synth = window.speechSynthesis; utterThis = new SpeechSynthesisUtterance(object_name + "Found"); 
+                synth.speak(utterThis); 
+            } 
+            else 
+            { 
+            document.getElementById("object_status").innerHTML = object_name + " Not Found"; 
+            }
+        }
+    }
 }
